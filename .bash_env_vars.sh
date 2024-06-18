e2314514cf9a2e5e08c9b88a7c42e0914b3f2e20
Onur Arıkan <onur93arikan@gmail.com>
2024-06-18 15:15:32 +0300

    Update fzf default options

--- .bash_env_vars.sh
+++ .bash_env_vars.sh
@@ -30,5 +30,5 @@ export WTTR_PARAMS="Fqm"
 export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # Open in tmux popup if on tmux, otherwise use --height mode
-export FZF_DEFAULT_OPTS='--height 40% --layout reverse --border top'
+export FZF_DEFAULT_OPTS="--height 40% --layout reverse --border top --preview 'bat --style=numbers --color=always --line-range :500 {}'"
 
