e5a05bc8ef001e55514ee1a50f2114278cbdc792
Onur Arıkan <onur93arikan@gmail.com>
2024-11-21 01:45:51 +0300

    Update mypy and FZF commands

--- .bash_env_vars.sh
+++ .bash_env_vars.sh
@@ -18,6 +18,7 @@ export PYTHONUTF8=1
 export PYTHONUNBUFFERED=1
 export PYTHONIOENCODING="utf8"
 export PYTHONPYCACHEPREFIX="${HOME}/.cache/pycache"
+export MYPY_CACHE_DIR="${HOME}/.cache/mypycache"
 export PYGAME_HIDE_SUPPORT_PROMPT="hide"
 export PYTHONSTARTUP=/gdrive/kod/py/__python_startup__.py
 
@@ -32,5 +33,12 @@ export WTTR_PARAMS="Fqm"
 export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # Open in tmux popup if on tmux, otherwise use --height mode
-export FZF_DEFAULT_OPTS="--height 40% --layout reverse --border top --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+export FZF_DEFAULT_OPTS="--height 60% --layout reverse --border top --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+# Print tree structure in the preview window
+export FZF_ALT_C_OPTS="
+  --walker-skip .git,node_modules,target
+  --preview 'tree -C {}'"
+# CTRL-Y to copy the command into clipboard using pbcopy
+export FZF_CTRL_R_OPTS="
+  --preview-window=hidden"
 export FZF_COMPLETION_TRIGGER='...'
