fbd0ce3b63d66d9df67dbc45186d408fb26364f7
Onur Arıkan <onur93arikan@gmail.com>
2025-11-29 17:32:52 +0100

    incline.nvim adjustments

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,3 +8,9 @@ vim.g.autoformat = false
 vim.opt.colorcolumn = "90"
 vim.opt.spelllang = { "en_us", "tr", "pl" }
 vim.g.root_spec = { "cwd" }
+
+-- views can only be fully collapsed with the global statusline
+vim.opt.laststatus = 3
+-- Default splitting will cause your main splits to jump when opening an edgebar.
+-- To prevent this, set `splitkeep` to either `screen` or `topline`.
+vim.opt.splitkeep = "screen"
